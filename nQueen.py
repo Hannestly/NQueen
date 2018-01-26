da21837088eba14d5da46dc49ba61cf44a546d21
Hyon Lee <dooley994@gmail.com>
2018-01-26 15:12:07 -0500

    fixed the errors

--- nQueen.py
+++ nQueen.py
@@ -1,16 +1,20 @@
+#edited by Stu Sully
+
+#The user inputs the dimension of the board. The dimension is equal to the number of queens placed 
 size = input("Enter the number of Queen")
 size = int(size)
 print("The size of the board: " + str(size))
 print("The number of Queen you're placing "+ str(size))
-queen = []
 
+#1D array is created based on the size input. The address of the array represent the column, and the element represesnt the row of that column
+#The initial position of the queens per each column are set at row 0
+queen = []
 for i in range(0,size):
     queen.append([])
     queen[i] = 0
 
+#The first Queen is set at column 0 and row 0, and it is safe, therefore we start the search for new safe spot in the column 1
 n = 1
-
-
 def solve(n): 
     #The base case: When we are dealing with queen > size of the board, means we have solved the problem
     if n == size:
@@ -34,7 +38,7 @@ def solve(n):
 
 def isSafe(n):
     #check if the row is safe
-    for rowCheck in range(0, size-1):
+    for rowCheck in range(0, n):
         if queen[rowCheck] == queen[n]:
             return False
     
@@ -63,11 +67,11 @@ def printBoard(size):
             board[rows][cols] = 0
 
     #set each occupied spot with 1, each column by column
-    for setCol in range(0,size-1):
-        board[queen[setCol]][setCol] == 1
+    for setCol in range(0,size):
+        board[queen[setCol]] [setCol] = 1
     
     #print the board, row by row
-    for printRow in range(0,size-1):
-        print("\n"+board[printRow] )
-    
+    for printRow in range(0,size):
+        print(board[printRow])
+
 solve(n)
